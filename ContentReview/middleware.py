c045ea26aa0e23bbbc48e9a8b0c46d4d7ec3ac31
ZhehaoZhao423 <zhehao4423@gmail.com>
2024-12-29 15:59:54 +0800

    apply DFA matching algorithm

--- ContentReview/middleware.py
+++ ContentReview/middleware.py
@@ -1,14 +1,100 @@
 # ContentReview/middleware.py
+# import re
+# from .sensitive_words import SENSITIVE_WORDS
+# from .sensitive_words import load_sensitive_words
 import re
-from .sensitive_words import SENSITIVE_WORDS
-from .sensitive_words import load_sensitive_words
-
+from collections import Counter
+from .sensitive_words import SensitiveWords
 # ContentReview/middleware.py
+
+class Node(object):
+    """字典树节点"""
+
+    def __init__(self):
+        self.children = None  # dict格式 {u'char1':node1, u'char2':node2...}
+        self.sensitive_word = None
+        self.sensitive_word_type = None
+
+class TextFilter(object):
+    """文本过滤"""
+
+    # Singleton
+    _instance = None
+
+    def __new__(cls, *args, **kw):
+        """单例模式"""
+        if not cls._instance:
+            cls._instance = super(TextFilter, cls).__new__(cls, *args, **kw)
+        return cls._instance
+
+    def __init__(self):
+        self.root = Node()
+        self.sensitive_word = SensitiveWords().sensitive_word_dict
+        for word_type in self.sensitive_word.keys():
+            for word in self.sensitive_word[word_type]:
+                self.add_word(word, word_type)
+
+    def add_word(self, word, word_type=u'common'):
+        """向字典树里添加敏感词汇及敏感词类型"""
+        # 向tire树添加节点
+        node = self.root
+        for i in range(len(word)):
+            if not node.children:  # 该节点是叶节点
+                node.children = {word[i]: Node()}
+            elif word[i] not in node.children:  # note : 监测dict中是否有某个key, 用 k in d 比用 k in d.keys() 快三倍
+                node.children[word[i]] = Node()
+            node = node.children[word[i]]
+        node.sensitive_word = word  # 在最后一个节点上记录整个词
+        node.sensitive_word_type = word_type
+
+    def is_contain(self, message):
+        """监测文本是否含有字典树的敏感词
+        返回一个列表,每一个元祖都是敏感词(出现在字符串文中的位置,敏感词,类型)"""
+        # 初始化结果变量
+        result = []
+        i, j, message_length = 0, 0, len(message)
+        # tire树 查找
+        while i < message_length:
+            j = i
+            p = self.root
+            while j < message_length and p.children is not None and message[j] in p.children:  # 匹配最长的词
+                p = p.children[message[j]]
+                j = j + 1
+            if p.sensitive_word:  # 查找时最后落到了敏感词叶节点上
+                result.append((j - len(p.sensitive_word),
+                               p.sensitive_word,
+                               p.sensitive_word_type))
+                i += len(p.sensitive_word)  # 直接跳跃到敏感词下一个字符进行继续匹配
+            else:
+                i += 1
+        return result
+
+    def filter(self, message, replace_char=u'*'):
+        """过滤文本,将其中的敏感词替换为过滤字符(默认为*)"""
+        res = self.is_contain(message)
+        for (i, word, _) in res:
+            message = message[:i] + u"".join([replace_char for _ in range(len(word))]) + message[i + len(word):]
+        return message
+
+    def classifie(self, message):
+        """过滤字符串,获取字符串分类及恶意程度"""
+        result = []
+        # 去除各种标点符号
+        message = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+", "", message)
+        res = self.is_contain(message)
+        # 聚合语句中的敏感词信息及权重
+        for _, _, word_type in res:
+            result.append(word_type)
+        d = dict(Counter(result))
+        d = sorted(d.items(), key=lambda x: x[1], reverse=True)
+        return d
+
 class SensitiveWordsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         # 在中间件初始化时加载敏感词
-        self.sensitive_words = load_sensitive_words('ContentReview/sensitive_words.txt')
+        # self.sensitive_words = load_sensitive_words('ContentReview/sensitive_words.txt')
+        self.textfilter = TextFilter()
 
     def __call__(self, request):
         if request.method == "POST":
@@ -17,15 +103,15 @@ class SensitiveWordsMiddleware:
 
             for key, value in mutable_post.items():
                 if isinstance(value, str):
-                    mutable_post[key] = self.censor_content(value)
+                    mutable_post[key] = self.textfilter.filter(value, replace_char='*')
 
             # 替换原始不可变 POST 数据
             request.POST = mutable_post
 
         return self.get_response(request)
 
-    def censor_content(self, content):
-        for word in SENSITIVE_WORDS:
-            pattern = re.compile(re.escape(word), re.IGNORECASE)
-            content = pattern.sub('**', content)
-        return content
+    # def censor_content(self, content):
+    #     for word in SENSITIVE_WORDS:
+    #         pattern = re.compile(re.escape(word), re.IGNORECASE)
+    #         content = pattern.sub('**', content)
+    #     return content
--- ContentReview/sensitive_words.py
+++ ContentReview/sensitive_words.py
@@ -1,21 +1,81 @@
-import re
+# import re
 
-# 定义一个函数来从文件加载敏感词列表
-def load_sensitive_words(filepath):
-    with open(filepath, 'r', encoding='utf-8') as f:
-        words = f.read().splitlines()
-    return words
+# # 定义一个函数来从文件加载敏感词列表
+# def load_sensitive_words(filepath):
+#     with open(filepath, 'r', encoding='utf-8') as f:
+#         words = f.read().splitlines()
+#     return words
+#
+# # 在需要使用的地方加载敏感词
+# SENSITIVE_WORDS = load_sensitive_words('ContentReview/sensitive_words.txt')
+#
+# def censor_content(content):
+#     """
+#     审查内容，替换敏感词为**
+#     :param content: 用户提交的内容
+#     :return: 审查后的内容
+#     """
+#     for word in SENSITIVE_WORDS:
+#         pattern = re.compile(re.escape(word), re.IGNORECASE)
+#         content = pattern.sub('**', content)
+#     return content
 
-# 在需要使用的地方加载敏感词
-SENSITIVE_WORDS = load_sensitive_words('ContentReview/sensitive_words.txt')
 
-def censor_content(content):
-    """
-    审查内容，替换敏感词为**
-    :param content: 用户提交的内容
-    :return: 审查后的内容
-    """
-    for word in SENSITIVE_WORDS:
-        pattern = re.compile(re.escape(word), re.IGNORECASE)
-        content = pattern.sub('**', content)
-    return content
+import os
+
+SENSITIVE_WORDS_DATA_PATH = "ContentReview/SensitiveWords/"  # 敏感词数据文件
+
+
+class SensitiveWords(object):
+    # Singleton
+    _instance = None
+
+    def __new__(cls, *args, **kw):
+        """单例模式"""
+        if not cls._instance:
+            cls._instance = super(SensitiveWords, cls).__new__(cls, *args, **kw)
+        return cls._instance
+
+    def __init__(self):
+        """构造函数:读取敏感词文件并初始化字典"""
+        global SENSITIVE_WORDS_DATA_PATH
+        self.file_name_list = os.listdir(SENSITIVE_WORDS_DATA_PATH)
+        self.file_path_list = map(lambda s: SENSITIVE_WORDS_DATA_PATH + s, self.file_name_list)
+        self.sensitive_word_dict = {}
+        for file_path in self.file_path_list:
+            self.get_sensitive_word(file_path)
+
+    def __str__(self):
+        """输出敏感词类详细信息"""
+        res = "共 " + str(len(self.sensitive_word_dict.keys())) + " 类 (" + ",".join(self.sensitive_word_dict.keys())
+        res += ")类敏感词:\n"
+        for k in self.sensitive_word_dict.keys():
+            res += k + "\t-\t" + str(len(self.sensitive_word_dict[k])) + " 个 \n"
+        return res
+
+    def get_sensitive_word(self, path):
+        """从文件中读取敏感词(每个一行)"""
+        global SENSITIVE_WORDS_DEFAULT_WEIGHT
+        with open(path, 'rb') as f:
+            sensitive_word_type = str(path).split('/')[-1].replace('.txt', '')
+            self.sensitive_word_dict[sensitive_word_type] = []
+            for line in f:
+                if line.strip():
+                    word = line.strip().decode('utf-8')
+                self.sensitive_word_dict[sensitive_word_type].append(word)
+
+    def add_sensitive_word(self, word, word_type='default', word_weight='10'):
+        """添加敏感词"""
+        if type(word) == str:  # str -> unicode
+            word = word.decode('utf-8')
+        if word_type in self.sensitive_word_dict.keys() or word_type == 'default':
+            self.sensitive_word_dict[word_type][word] = word_weight
+
+    def save_data(self):
+        """存储数据到文件中(读取地址)"""
+        for word_type in self.sensitive_word_dict.keys():
+            file_path = filter(lambda x: word_type in x, self.file_path_list)[0]
+            with open(file_path, 'wb') as f:
+                for word in self.sensitive_word_dict[word_type]:
+                    f.write(word.encode("utf-8"))
+                    f.write("\n")
